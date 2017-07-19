1824a336ec8a48ae78e955253d12d56dcdb3e40b
leaaah <lgmoore23@gmail.com>
2017-07-19 15:32:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function mychangecolor() {
+    var x = document.getElementById("Name").value;
+    document.getElementById("word1").innerHTML = x;
+}
+function mychangecolor2() {
+    var x = document.getElementById("Verb").value;
+    document.getElementById("word2").innerHTML = x;
+}
+function mychangecolor3() {
+    var x = document.getElementById("Adverb").value;
+    document.getElementById("word3").innerHTML = x;
+}
+function mychangecolor4() {
+    var x = document.getElementById("Verb2").value;
+    document.getElementById("word4").innerHTML = x;
+}
+function mychangecolor5() {
+    var x = document.getElementById("Noun").value;
+    document.getElementById("word5").innerHTML = x;
+}
+function mychangecolor6() {
+    var x = document.getElementById("Adjective2").value;
+    document.getElementById("word6").innerHTML = x;
+}
+function mychangecolor7() {
+    var x = document.getElementById("Adverb2").value;
+    document.getElementById("word7").innerHTML = x;
+}
+function mychangecolor8() {
+    var x = document.getElementById("Noun2").value;
+    document.getElementById("word8").innerHTML = x;
+}
