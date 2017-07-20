2a72b838f2349e6b61f0620df5b3f3bdb4f7f021
leaaah <lgmoore23@gmail.com>
2017-07-20 13:15:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 function mychangecolor() {
-    var x = document.getElementById("Name").value;
-    document.getElementById("word1").innerHTML = x;
-}
-function mychangecolor2() {
-    var x = document.getElementById("Verb").value;
-    document.getElementById("word2").innerHTML = x;
-}
-function mychangecolor3() {
-    var x = document.getElementById("Adverb").value;
-    document.getElementById("word3").innerHTML = x;
-}
-function mychangecolor4() {
-    var x = document.getElementById("Verb2").value;
-    document.getElementById("word4").innerHTML = x;
-}
-function mychangecolor5() {
-    var x = document.getElementById("Noun").value;
-    document.getElementById("word5").innerHTML = x;
-}
-function mychangecolor6() {
-    var x = document.getElementById("Adjective2").value;
-    document.getElementById("word6").innerHTML = x;
-}
-function mychangecolor7() {
-    var x = document.getElementById("Adverb2").value;
-    document.getElementById("word7").innerHTML = x;
-}
-function mychangecolor8() {
-    var x = document.getElementById("Noun2").value;
-    document.getElementById("word8").innerHTML = x;
+    var a = document.getElementById("Name").value;
+    document.getElementById("word1").innerHTML = a;
+
+    var b = document.getElementById("Verb").value;
+    document.getElementById("word2").innerHTML = b;
+
+    var c = document.getElementById("Adverb").value;
+    document.getElementById("word3").innerHTML = c;
+
+    var d = document.getElementById("Verb2").value;
+    document.getElementById("word4").innerHTML = d;
+
+    var e = document.getElementById("Noun").value;
+    document.getElementById("word5").innerHTML = e;
+
+    var f = document.getElementById("Adjective2").value;
+    document.getElementById("word6").innerHTML = f;
+
+    var g = document.getElementById("Adverb2").value;
+    document.getElementById("word7").innerHTML = g;
+
+    var h = document.getElementById("Name2").value;
+    document.getElementById("word8").innerHTML = h;
+
+    document.getElementById("lineone").style.display = 'block';
+    document.getElementById("linetwo").style.display = 'block';
+    document.getElementById("linethree").style.display = 'block';
 }
